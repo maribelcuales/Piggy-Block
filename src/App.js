c7725f74ee9c1c557a9722a1831af2a6bba9a292
maribelcuales <maribelcuales@gmail.com>
2022-09-22 09:01:11 +0800

    Import Browserouter, routes, and route.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './assets/piggy-block-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2 className='landing-title'>
-          Launching Soon! 
-        </h2>
-      </header>
+      <BrowserRouter>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2 className='landing-title'>
+            Launching Soon!
+          </h2>
+          <Routes>
+            <Route path='/' />
+          </Routes>
+        </header>
+      </BrowserRouter>
     </div>
   );
 }
